a9f3beba57d0eb8470c82b1514830f403f3d2840
sakshikhule <102469697+sakshikhule@users.noreply.github.com>
2023-01-31 17:07:18 +0530

    Add files via upload

---
+++ day 59.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int t;
+ cout<<"enter no =";
+cin>>t;
+ while(t--){
+ int m,h;
+ cout<<"enter any two no=";
+cin>>m>>h;
+int BMI=m/(h*h);
+ if (BMI<=18){
+cout<<1<<endl;
+ }
+ else if(BMI>18&&BMI<=24){
+cout<<2<<endl;
+ }
+ else if(BMI>24&&BMI<=29){
+cout<<3<<endl;
+ }
+else{
+cout<<4<<endl;
+ }
+ }
+return 0;
+}
